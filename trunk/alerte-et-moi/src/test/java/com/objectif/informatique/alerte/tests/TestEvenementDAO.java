72aff2704a3b7e5133d8b7a55147ab8617ba9c6b
vdibi <vdibi@63c3d20e-893b-460d-95bf-c5867539e51c>
2014-06-13 13:33:29 +0000

    Mise en place des JUnit test

---
+++ trunk/alerte-et-moi/src/test/java/com/objectif/informatique/alerte/tests/TestEvenementDAO.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.objectif.informatique.alerte.tests;
+
+import com.objectif.informatique.alerte.dao.EvenementDAO;
+
+import junit.framework.TestCase;
+
+/**
+ * @author vdibi
+ *
+ */
+public class TestEvenementDAO extends TestCase {
+	EvenementDAO evenementDAO;
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
